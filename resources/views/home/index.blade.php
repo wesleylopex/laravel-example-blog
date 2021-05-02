630df0c4014b016b10f769a714aeb458eaf65123
Wesley Lopes <x.wesleylopes@gmail.com>
2021-05-02 18:31:31 -0300

    feat: create blog post design

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,74 @@
 @extends('layouts/app')
 
 @section('content')
-    <div>
-        <img class="w-20" src="{{ asset('images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg') }}" alt="">
-    </div>
+    <section class="py-12 bg-gray-100">
+        <div class="max-w-screen-xl mx-auto">
+            <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div class="p-4 bg-white shadow-md rounded-sm">
+                    <a href="#">
+                        <figure class="overflow-hidden rounded-sm">
+                            <img class="transition-transform duration-200 object-cover transform hover:scale-110 rounded-sm" src="{{ asset('images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg') }}" alt="Computador minimalista">
+                        </figure>
+                    </a>
+                    <a href="#">
+                        <h2 class="mt-2 text-gray-800 hover:underline text-lg font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                    </a>
+                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, dolore molestias ducimus, similique.</p>
+                    <a href="#">
+                        <button class="mt-2 hover:underline text-blue-600">
+                            Ver mais
+                        </button>
+                    </a>
+                </div>
+                <div class="p-4 bg-white shadow-md rounded-sm">
+                    <a href="#">
+                        <figure class="overflow-hidden rounded-sm">
+                            <img class="transition-transform duration-200 object-cover transform hover:scale-110 rounded-sm" src="{{ asset('images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg') }}" alt="Computador minimalista">
+                        </figure>
+                    </a>
+                    <a href="#">
+                        <h2 class="mt-2 text-gray-800 hover:underline text-lg font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                    </a>
+                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, dolore molestias ducimus, similique.</p>
+                    <a href="#">
+                        <button class="mt-2 hover:underline text-blue-600">
+                            Ver mais
+                        </button>
+                    </a>
+                </div>
+                <div class="p-4 bg-white shadow-md rounded-sm">
+                    <a href="#">
+                        <figure class="overflow-hidden rounded-sm">
+                            <img class="transition-transform duration-200 object-cover transform hover:scale-110 rounded-sm" src="{{ asset('images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg') }}" alt="Computador minimalista">
+                        </figure>
+                    </a>
+                    <a href="#">
+                        <h2 class="mt-2 text-gray-800 hover:underline text-lg font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                    </a>
+                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, dolore molestias ducimus, similique.</p>
+                    <a href="#">
+                        <button class="mt-2 hover:underline text-blue-600">
+                            Ver mais
+                        </button>
+                    </a>
+                </div>
+                <div class="p-4 bg-white shadow-md rounded-sm">
+                    <a href="#">
+                        <figure class="overflow-hidden rounded-sm">
+                            <img class="transition-transform duration-200 object-cover transform hover:scale-110 rounded-sm" src="{{ asset('images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg') }}" alt="Computador minimalista">
+                        </figure>
+                    </a>
+                    <a href="#">
+                        <h2 class="mt-2 text-gray-800 hover:underline text-lg font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                    </a>
+                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, dolore molestias ducimus, similique.</p>
+                    <a href="#">
+                        <button class="mt-2 hover:underline text-blue-600">
+                            Ver mais
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
