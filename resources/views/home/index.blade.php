a9c90e1383845504e7d81e5d6bc186dbbf5b455b
Wesley Lopes <x.wesleylopes@gmail.com>
2021-05-02 18:15:59 -0300

    chore: add tailwindcss and blade layouts

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,7 @@
-Home
+@extends('components/layout')
+
+@section('content')
+    <div>
+        <img class="w-20" src="{{ asset('images/ales-nesetril-Im7lZjxeLhg-unsplash.jpg') }}" alt="">
+    </div>
+@endsection
